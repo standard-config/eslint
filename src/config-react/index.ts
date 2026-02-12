804a984e64f30001019c7255a661e302d164967b
Dom Porada <dom@dom.engineering>
2026-02-12 14:34:35 +0100

    Enable `eslint-plugin-react-x`

--- src/config-react/index.ts
+++ src/config-react/index.ts
@@ -3,6 +3,7 @@ import type { LinterConfigEntry } from '../types/index.d.ts';
 import pluginPerfectionist from 'eslint-plugin-perfectionist';
 import pluginReact from 'eslint-plugin-react';
 import pluginReactHooks from 'eslint-plugin-react-hooks';
+import pluginReactX from 'eslint-plugin-react-x';
 
 /**
  * This config is intentionally limited to rules not supported by Oxlint
@@ -14,6 +15,7 @@ const config: LinterConfigEntry = {
 		'perfectionist': pluginPerfectionist,
 		'react': pluginReact,
 		'react-hooks': pluginReactHooks as ESLint.Plugin,
+		'react-x': pluginReactX,
 	},
 	settings: {
 		react: {
@@ -63,7 +65,28 @@ const config: LinterConfigEntry = {
 				type: 'unsorted',
 			},
 		],
-		'react/destructuring-assignment': ['error', 'always'],
+		'react-x/jsx-dollar': 'error',
+		'react-x/jsx-key-before-spread': 'error',
+		'react-x/jsx-no-iife': 'error',
+		'react-x/no-access-state-in-setstate': 'error',
+		'react-x/no-class-component': 'error',
+		'react-x/no-context-provider': 'error',
+		'react-x/no-default-props': 'error',
+		'react-x/no-duplicate-key': 'error',
+		'react-x/no-forward-ref': 'error',
+		'react-x/no-implicit-key': 'error',
+		'react-x/no-leaked-conditional-rendering': 'error',
+		'react-x/no-misused-capture-owner-stack': 'error',
+		'react-x/no-nested-component-definitions': 'error',
+		'react-x/no-nested-lazy-component-declarations': 'error',
+		'react-x/no-prop-types': 'error',
+		'react-x/no-unnecessary-use-callback': 'error',
+		'react-x/no-unnecessary-use-memo': 'error',
+		'react-x/no-unstable-context-value': 'error',
+		'react-x/no-unstable-default-props': 'error',
+		'react-x/no-use-context': 'error',
+		'react-x/prefer-destructuring-assignment': 'error',
+		'react-x/prefer-use-state-lazy-initialization': 'error',
 		'react/function-component-definition': [
 			'error',
 			{
@@ -71,10 +94,7 @@ const config: LinterConfigEntry = {
 				unnamedComponents: 'arrow-function',
 			},
 		],
-		'react/hook-use-state': 'error',
-		'react/jsx-no-constructed-context-values': 'error',
 		'react/no-adjacent-inline-elements': 'error',
-		'react/no-unstable-nested-components': 'error',
 
 		// Enforce all `react-hooks` rules as errors
 		...Object.fromEntries(
