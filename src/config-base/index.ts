f17ca0c1832ef50e402900f286c89df10199dfe3
Dom Porada <dom@dom.engineering>
2026-02-13 21:46:59 +0100

    Enable `@stylistic/eslint-plugin`

--- src/config-base/index.ts
+++ src/config-base/index.ts
@@ -1,4 +1,5 @@
 import type { LinterConfigEntry } from '../types/index.d.ts';
+import pluginStylistic from '@stylistic/eslint-plugin';
 import pluginPerfectionist from 'eslint-plugin-perfectionist';
 import tseslint from 'typescript-eslint';
 
@@ -9,6 +10,7 @@ import tseslint from 'typescript-eslint';
 const config: LinterConfigEntry = {
 	name: 'Base Config',
 	plugins: {
+		'@stylistic': pluginStylistic,
 		'@typescript-eslint': tseslint.plugin,
 		'perfectionist': pluginPerfectionist,
 	},
@@ -34,6 +36,87 @@ const config: LinterConfigEntry = {
 				{ considerPropertyDescriptor: true },
 			],
 		},
+		'@stylistic/lines-between-class-members': [
+			'error',
+			'always',
+			{ exceptAfterSingleLine: true },
+		],
+		'@stylistic/padding-line-between-statements': [
+			'error',
+			{
+				blankLine: 'always',
+				next: '*',
+				prev: [
+					'block-like',
+					'directive',
+					'export',
+					'function',
+					'import',
+					'interface',
+					'type',
+				],
+			},
+			{
+				blankLine: 'always',
+				next: [
+					'block-like',
+					'directive',
+					'export',
+					'function',
+					'import',
+					'interface',
+					'type',
+				],
+				prev: '*',
+			},
+			{
+				blankLine: 'always',
+				next: '*',
+				prev: ['case', 'default'],
+			},
+			{
+				blankLine: 'never',
+				next: 'directive',
+				prev: 'directive',
+			},
+			{
+				blankLine: 'any',
+				next: 'export',
+				prev: 'export',
+			},
+			{
+				blankLine: 'never',
+				next: ['function', 'function-overload'],
+				prev: 'function-overload',
+			},
+			{
+				blankLine: 'never',
+				next: 'import',
+				prev: 'import',
+			},
+			{
+				blankLine: 'any',
+				next: 'interface',
+				prev: 'interface',
+			},
+			{
+				blankLine: 'any',
+				next: 'type',
+				prev: 'type',
+			},
+		],
+		'@stylistic/spaced-comment': [
+			'error',
+			'always',
+			{
+				block: {
+					balanced: true,
+				},
+				line: {
+					markers: ['/'],
+				},
+			},
+		],
 		'@typescript-eslint/consistent-type-assertions': [
 			'error',
 			{
