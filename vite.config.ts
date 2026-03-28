725c07a6a46865f6bc341903af5e9ea0bfc5da1c
Dom Porada <dom@dom.engineering>
2026-03-28 12:00:35 +0100

    Update dependencies

---
+++ vite.config.ts
@@ -0,0 +1,52 @@
+import { defineConfig } from 'vite-plus';
+import {
+	defineOxlintConfig,
+	oxlintConfigConfigFiles,
+} from './src/utilities.ts';
+
+export default defineConfig({
+	test: {
+		setupFiles: ['vitest.setup.ts'],
+		typecheck: {
+			enabled: true,
+		},
+	},
+	lint: defineOxlintConfig({
+		rules: {
+			'typescript/no-deprecated': 'off',
+		},
+		overrides: [
+			{
+				files: [
+					/* prettier-ignore */
+					'src/config-*/index.ts',
+					'src/config-*/rules-*.ts',
+				],
+				...oxlintConfigConfigFiles,
+			},
+		],
+	}),
+	pack: {
+		deps: {
+			skipNodeModulesBundle: true,
+		},
+		dts: {
+			sourcemap: true,
+		},
+		entry: [
+			/* prettier-ignore */
+			'src/index.ts',
+			'src/utilities.ts',
+		],
+		failOnWarn: true,
+		publint: true,
+		sourcemap: true,
+	},
+	staged: {
+		'*': [
+			() => 'pnpm install --ignore-scripts',
+			'prettier --ignore-unknown --write',
+			() => 'pnpm prepack',
+		],
+	},
+});
